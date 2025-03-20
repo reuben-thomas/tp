6b1662d58476b95fafe3b3fd252f849da0a2f766
Reuben Thomas <reubenthomas123@gmail.com>
2025-03-20 23:18:20 +0800

    Remove unused import from FindBy test

--- src/test/java/seedu/address/logic/commands/FindByCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindByCommandTest.java
@@ -1,16 +1,7 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-import static seedu.address.logic.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.CARL;
-import static seedu.address.testutil.TypicalPersons.ELLE;
-import static seedu.address.testutil.TypicalPersons.FIONA;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 
