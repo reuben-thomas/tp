5fb2c1647091945a226945172c446fc8cb68a20e
gajinkee <gajinkee@gmail.com>
2025-03-26 16:04:36 +0800

    Icons: fixed

--- src/main/java/seedu/address/ui/UnicodeIcons.java
+++ src/main/java/seedu/address/ui/UnicodeIcons.java
@@ -4,16 +4,16 @@ package seedu.address.ui;
  * A useful set of Unicode icons for use in the UI.
  */
 public class UnicodeIcons {
-    public static final String CALENDAR = "\uD83D\uDCC5";  // 📅
-    public static final String EMAIL = "\u2709";          // ✉
-    public static final String NAME_CARD = "\uD83D\uDCC7"; // 📇
-    public static final String PHONE = "\u260E";          // ☎
-    public static final String HOME = "\uD83C\uDFE0";     // 🏠
-    public static final String LAPTOP = "\uD83D\uDCBB";   // 💻
-    public static final String TAG = "\uD83C\uDFF7";      // 🏷
-    public static final String CHECK = "\u2714";         // ✔
-    public static final String CLOCK = "\u23F0";         // ⏰
-    public static final String WRENCH_CLOCK = "\uD83D\uDD27"; // 🔧
-    public static final String COG_PAUSE = "\u2699";         // ⚙
-    public static final String BUSINESS_TIME = "\uD83D\uDCBC"; // 💼
+    public static final String CALENDAR = "\uD83D\uDCC5";
+    public static final String EMAIL = "\uD83D\uDCE9";
+    public static final String NAME_CARD = "\uD83D\uDCC7";
+    public static final String PHONE = "\uD83D\uDCDE";
+    public static final String HOME = "\uD83C\uDFE0";
+    public static final String LAPTOP = "\uD83D\uDCBB";
+    public static final String TAG = "\uD83C\uDFF7";
+    public static final String CHECK = "\u2714";
+    public static final String CLOCK = "\u23F0";
+    public static final String WRENCH_CLOCK = "\uD83D\uDD27";
+    public static final String COG_PAUSE = "\u2699";
+    public static final String BUSINESS_TIME = "\uD83D\uDCBC";
 }
