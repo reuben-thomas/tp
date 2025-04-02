9c050ed26e2d7d3dfd9ab08dd74942dd185ac857
gajinkee <gajinkee@gmail.com>
2025-04-03 00:02:38 +0800

    PersonList: Stays expanded with delete

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -1,14 +1,19 @@
 package seedu.address.ui;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.person.OrgID;
 import seedu.address.model.person.Person;
 
 /**
@@ -17,6 +22,7 @@ import seedu.address.model.person.Person;
 public class PersonListPanel extends UiPart<Region> {
     private static final String FXML = "PersonListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
+    private final Map<OrgID, Boolean> expandedCells = new HashMap<>();
 
     @FXML
     private ListView<Person> personListView;
@@ -35,6 +41,7 @@ public class PersonListPanel extends UiPart<Region> {
      * a {@code PersonCard}.
      */
     class PersonListViewCell extends ListCell<Person> {
+
         @Override
         protected void updateItem(Person person, boolean empty) {
             super.updateItem(person, empty);
@@ -44,19 +51,23 @@ public class PersonListPanel extends UiPart<Region> {
                 setText(null);
             } else {
                 PersonCard personCard = new PersonCard(person, getIndex() + 1);
-                preserveExpandedState(personCard);
+                preserveExpandedState(personCard, person.getOrgID());
                 setGraphic(personCard.getRoot());
+
+                // Listen for expansion changes and update map
+                TitledPane titledPane = (TitledPane) personCard.getRoot().lookup("#cardPaneTitledPane");
+                if (titledPane != null) {
+                    titledPane.expandedProperty().addListener((obs, oldVal, newVal) -> {
+                        expandedCells.put(person.getOrgID(), newVal);
+                    });
+                }
+
             }
         }
 
-        private void preserveExpandedState(PersonCard newCard) {
-            boolean isExpanded = false;
-            if (getGraphic() instanceof Region) {
-                TitledPane currentTitledPane = (TitledPane) getGraphic().lookup("#cardPaneTitledPane");
-                if (currentTitledPane != null) {
-                    isExpanded = currentTitledPane.isExpanded();
-                }
-            }
+        private void preserveExpandedState(PersonCard newCard, OrgID id) {
+
+            boolean isExpanded = expandedCells.getOrDefault(id, false);
 
             TitledPane newTitledPane = (TitledPane) newCard.getRoot().lookup("#cardPaneTitledPane");
             if (newTitledPane != null) {
