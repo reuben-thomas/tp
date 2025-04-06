93ed8a8d4dbbbe14281d84715960be64bba6d3e0
Reuben Thomas <reubenthomas123@gmail.com>
2025-04-06 20:36:03 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -16,7 +16,6 @@ import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.LogOutCommand;
-import seedu.address.logic.commands.LoginCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.commands.exceptions.CreateUserException;
 import seedu.address.logic.commands.exceptions.InvalidAccessRightsException;
