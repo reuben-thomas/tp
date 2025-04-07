2e635e8b22ea4c80ef73c96cabd3c463a1878c44
Reuben Thomas <reubenthomas123@gmail.com>
2025-04-08 01:17:47 +0800

    Add command parser test

---
+++ src/test/java/seedu/address/logic/parser/FindByCommandParserTest.java
@@ -0,0 +1,43 @@
+package seedu.address.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.FindByCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class FindByCommandParserTest {
+    private FindByCommandParser parser = new FindByCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        ParseException exception = assertThrows(ParseException.class, () -> parser.parse("     "));
+        assertEquals(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindByCommand.MESSAGE_USAGE),
+                exception.getMessage());
+    }
+
+    @Test
+    public void parse_nonEmptyPreamble_throwsParseException() {
+        ParseException exception = assertThrows(ParseException.class, () -> parser.parse("some preamble n/Alice"));
+        assertEquals(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindByCommand.MESSAGE_USAGE),
+                exception.getMessage());
+    }
+
+    @Test
+    public void parse_emptyPrefixValue_throwsParseException() {
+        for (Prefix prefix : FindByCommandParser.getPrefixes()) {
+            ParseException exception = assertThrows(ParseException.class, () -> parser.parse(prefix.getPrefix() + " "));
+            assertEquals(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindByCommand.MESSAGE_USAGE),
+                    exception.getMessage());
+        }
+    }
+}
